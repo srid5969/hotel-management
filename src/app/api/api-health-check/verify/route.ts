5e637a6773d386369916b3b9473ef4af0a1867c4
Sridhar R <sridhar@gem3s.in>
2023-11-23 13:11:28 +0530

    removed handler

--- src/app/api/api-health-check/verify/route.ts
+++ src/app/api/api-health-check/verify/route.ts
@@ -1,21 +1,7 @@
 import { InternalErrorResponse, SuccessResponse } from '@/util/apiResponse';
-import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 
-export async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const taskName = 'HEALTH-CHECK';
-  try {
-    //returns the success response
-    res.status(200).json({ message: 'GET request handled successfully' });
-
-    // new SuccessResponse(res, 'success', 'api(s) is working...').send();
-  } catch (err) {
-    console.error(`${taskName}_ERROR`, err);
-    const response = new InternalErrorResponse(res, 'An unexpected error has occurred');
-    response.send();
-  }
-}
-export async function GET()   {
+export async function GET() {
   const taskName = 'HEALTH-CHECK';
   const res: any = NextResponse;
   try {
