d7775492acceedb7b437d63692bfc91548b949f3
Sridhar R <sridhar@gem3s.in>
2023-11-24 17:35:09 +0530

    added post api

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,9 +1,16 @@
 import { prisma } from "@/config/db";
+import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
 
 export async function GET() {
     const allUsers = await prisma.user.findMany()
+
+    return NextResponse.json({data:allUsers})
+    //http://localhost:3000/api/user {data:"all"}
+}
+export async function POST(request:NextApiRequest) {
+    const allUsers = await prisma.user.create({data:{  }})
 
     return NextResponse.json({data:allUsers})
     //http://localhost:3000/api/user {data:"all"}
