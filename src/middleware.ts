f309dccd719856cece11977a1a3478368e09810e
Sridhar R <sridhar@gem3s.in>
2023-11-24 17:16:52 +0530

    lint errors fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,8 +17,6 @@ export async function middleware(request: NextRequest) {
   const authNotRequuiredUrls=['/api/api-health-check/verify']
   if(authNotRequuiredUrls.includes(url)) return NextResponse.next()
   let accessToken = request.headers.get('authorization')
-
-
               if (!accessToken) {
                 return NextResponse.json({data:null,message:"Token not found"},{status:404})
               }
