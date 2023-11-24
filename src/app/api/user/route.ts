756d8828c577dae55dd6b2052907262249ed1b8e
Sridhar R <sridhar@gem3s.in>
2023-11-24 17:22:48 +0530

    moved prima cint to config folder

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,9 +1,10 @@
+import { prisma } from "@/config/db";
 import { Prisma, PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 
 export async function GET() {
-    const allUsers = await new PrismaClient().user.findMany()
+    const allUsers = await prisma.user.findMany()
 
     return NextResponse.json({data:allUsers})
     //http://localhost:3000/api/user {data:"all"}
