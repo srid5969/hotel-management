65e15c8b4d79945afd76cee77ffce3c0c51a8720
Sridhar R <sridhar@gem3s.in>
2023-11-30 17:21:20 +0530

    updated swagger config

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,6 +1,7 @@
 import {NextRequest, NextResponse} from 'next/server';
 
 export async function GET(request: NextRequest, context: any) {
+  request.json()
   return NextResponse.json({data: {param: context.params}});
   // http://localhost:3000/api/user/1 {"data":{"param":{"id":"1"}}}
 }
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -24,6 +24,68 @@ export async function GET() {
   return NextResponse.json({data: allUsers});
   //http://localhost:3000/api/user {data:"all"}
 }
+/**
+ * @swagger
+ * tags:
+ *   - name: User
+ *     description: Operations related to users
+ * /api/user:
+ *   post:
+ *     tags:
+ *       - User
+ *     summary: Create a new user
+ *     description: Endpoint to create a new user.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 default: example@gmail.com
+ *               phoneNumber:
+ *                 type: string
+ *                 default: 1234-123-1234
+ *               password:
+ *                 type: string
+ *                 default: Test@123
+ *       headers:
+ *         X-Custom-Header:
+ *           description: Custom header description
+ *           schema:
+ *             type: string
+ *           example: CustomHeaderValue123
+ *     responses:
+ *       200:
+ *         description: Successful creation of a user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                     email:
+ *                       type: string
+ *                     phoneNumber:
+ *                       type: string
+ *               example:
+ *                 message: User created successfully
+ *                 data:
+ *                   id: 12
+ *                   email: example@gmail.com
+ *                   phoneNumber: 1234-123-1234
+ *       400:
+ *         description: Bad request or invalid input data
+ */
+
 export async function POST(request: NextRequest) {
   try {
     const body: MyData = await request.json(); // This get's the raw body as a string
