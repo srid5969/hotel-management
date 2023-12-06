32db62cec11a407cc4b87c1e1789b06846f295ce
Sridhar R <sridhar@gem3s.in>
2023-12-06 13:25:25 +0530

    post api

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,42 @@
-'use client'; // This is a client component 👈🏽
-import React, {useState} from 'react';
-export default function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+'use client';
+
+import {FormEvent} from 'react';
+
+export default function Login() {
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+
+    const response = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({email: email, password: password}),
+    });
+
+    // Handle response if necessary
+    const data = await response.json();
+    // ...
+    return data;
+  }
   return (
-    <form action="/api/auth/login" method="POST">
-      <h2>Login</h2>
-      <div>
-        <label>Username:</label>
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+    <>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>email</label>
+          <input type="text" name="email" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" name="password" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </>
   );
 }
